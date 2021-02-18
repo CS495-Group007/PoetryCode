affda965663b339575346bf749e9bfa3c935fb52
maddiecmac <madison_macaulay@hotmail.com>
2021-02-18 14:34:30 -0600

    separating css files

--- poetry-app/src/App.js
+++ poetry-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './SimplyScansion.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap"
 
@@ -18,6 +19,8 @@ function App() {
           <Nav>
             <Nav.Link href="/sign-in">Log In</Nav.Link>
             <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+            <Nav.Link href="/simply-scansion">Scan a Poem</Nav.Link>
+            <Nav.Link href="#">Add a Poem</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
