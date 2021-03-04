1b224f1b4c156cce8e9fac0ca7ddd332ce64657f
Connor <chmeadows2@crimson.ua.edu>
2021-03-04 16:13:06 -0600

    tabular layout for the list

--- poetry-app/src/Components/Dashboards/StudentDashboard.js
+++ poetry-app/src/Components/Dashboards/StudentDashboard.js
@@ -2,30 +2,89 @@ import React from 'react';
 import '../../Styling/DashboardStyling.css';
 // import SortChooser from './SortChooser'
 
+// DB Constants so regardless of what we put in the DB I won't have to change these a lot of places
+const completePoem = "complete", 
+      incompletePoem = "inProgress",
+      studentIndicator = "student",
+      instructorIndicator = "instructor";
 
+
+// Dummy data for testing
 var studentPoems = {
     "Shakespeare" : {
-        "Sonnet One": {
-            "status": "c"
-        },
         "Sonnet Two": {
-            "status": "i",
+            "status": completePoem
+        },
+        "Sonnet Four": {
+            "status": incompletePoem,
+            "currentResponse": "dactyl spondee iamb iamb trochee"
+        }
+    },
+    "Catullus" : {
+        "Carmen 1" : {
+            "status" : incompletePoem,
             "currentResponse": "dactyl spondee iamb iamb trochee"
         }
     }
 };
 
+var allPoems = {
+    "Shakespeare" : {
+        "Sonnet One" : {
+
+        },
+        "Sonnet Two" : {
+
+        },
+        "Sonnet Three" : {
+
+        },
+        "Sonnet Four" : {
+
+        }
+    },
+    "Catullus" : {
+        "Carmen 1" : {
+
+        },
+        "Carmen 2" : {
+
+        },
+        "Carmen 3" : {
+
+        },
+        "Carmen 4" : {
+            
+        },
+        "Carmen 5" : {
+
+        },
+        "Carmen 6" : {
+            
+        },
+        "Carmen 7" : {
+
+        },
+        "Carmen 8" : {
+            
+        }
+    }
+}
+
 var studentName = "Connor";
-var role = "s";
+var role = studentIndicator;
+
+// End of dummy data
 
 
 class StudentDashboard extends React.Component {
 
+    // Translators turn DB information into aesthetic equivalents
     translateStatus(status) {
         switch(status) {
-            case('i'):
+            case(incompletePoem):
                 return "In Progress";
-            case('c'):
+            case(completePoem):
                 return "Finished";
             default:
                 return "";
@@ -34,9 +93,9 @@ class StudentDashboard extends React.Component {
 
     translateRole(role) {
         switch(role){
-            case('s'):
+            case(studentIndicator):
                 return "Student";
-            case('i'):
+            case(instructorIndicator):
                 return "Instructor";
             default:
                 return ""
@@ -45,15 +104,53 @@ class StudentDashboard extends React.Component {
 
     render() {
 
-        var poemsList = [];
+        var poemsTable = [];
 
         for (var poet in studentPoems) {
             for (var p in studentPoems[poet]) {
-                console.log(p)
-                poemsList.push(
-                    <div className="listedPoem">
-                        <div className="listedPoemTitle"> {p} </div>
-                        <div className="listedPoemProgress"> { this.translateStatus(studentPoems[poet][p]["status"]) } </div>
+                var classnameList = ["poemTableCell"];
+                switch(studentPoems[poet][p]["status"]){
+                    case incompletePoem:
+                        classnameList.push("incompletePoem");
+                        break;
+                    case completePoem:
+                        classnameList.push("completePoem");
+                        break;
+                    default:
+                        classnameList.push("unstartedPoem");
+                        break;
+                }
+
+                poemsTable.push(
+                    <div className="poemTableRow">
+                        <div className={classnameList.join(' ')}>
+                            <div className="singlePoemTable">
+                                    <div className="singlePoemTableRow">
+                                        <div className="singlePoemTableCell listedPoemTitle"> {p} </div>
+                                        <div className="singlePoemTableCell listedPoemAuthor"> {poet} </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                );
+            }
+        }
+
+        for (poet in allPoems) {
+            for (p in allPoems[poet]) {
+                if(studentPoems[poet] && studentPoems[poet][p]) {
+                    continue
+                }
+                poemsTable.push(
+                    <div className="poemTableRow">
+                        <div className="poemTableCell">
+                            <div className="singlePoemTable">
+                                <div className="singlePoemTableRow">
+                                    <div className="singlePoemTableCell listedPoemTitle"> {p} </div>
+                                    <div className="singlePoemTableCell listedPoemAuthor"> {poet} </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 );
             }
@@ -68,8 +165,10 @@ class StudentDashboard extends React.Component {
                 <div className="regularDashboardLabel">
                     My Poems
                 </div>
-                
-                {poemsList}
+
+                <div className="poemTable">
+                    { poemsTable }
+                </div>
 
             </div>
         );
