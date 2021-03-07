1146b68c85cc7c6f83cb101efa3f4237503d6d51
jstucker2 <jstucker2@crimson.ua.edu>
2021-03-07 01:38:34 -0600

    Made each line of the poem display correctly

--- poetry-app/src/Components/PoemSelector.js
+++ poetry-app/src/Components/PoemSelector.js
@@ -11,20 +11,26 @@ class PoemSelector extends React.Component{
 
     render(){
         let rows = []
+        let poemLines = []
         rows.push("");
         rows.push(<option selected disabled>Select A Poem</option>)
         let poemsAsArray = Object.keys(this.props.poems).map((pid) => this.props.poems[pid]);
         poemsAsArray.forEach((poem) => {
-            rows.push(<option value={poem.id}>{poem.name}</option>)
+            rows.push(<option value={poem.id}>{poem.name}</option>);
         });
         poemsAsArray.unshift("");
+        if(this.props.currentPoemNumber > 0){
+            poemsAsArray[this.props.currentPoemNumber].text.forEach((poem) => {
+                poemLines.push(<div>{poem}</div>);
+            });
+        }
         return(
             <div>
                 <select value = {this.currentPoemName} onChange = {this.handleChange}>
                     {rows}
                 </select>
                 <blockquote>
-                        {poemsAsArray[this.props.currentPoemNumber].text}
+                    {poemLines}
                 </blockquote>
                 <cite> {poemsAsArray[this.props.currentPoemNumber].poet} </cite>
             </div>
