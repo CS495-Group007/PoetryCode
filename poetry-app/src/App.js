24febe02b96fbb62f6dcbb5b2c139db0d550d366
jstucker2 <jstucker2@crimson.ua.edu>
2021-03-19 19:18:49 -0500

    Removed Scan a Poem from nav bar

--- poetry-app/src/App.js
+++ poetry-app/src/App.js
@@ -7,7 +7,6 @@ import { Nav, Navbar } from "react-bootstrap";
 //Import the components to set up the navigation bar
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
-import SimplyScansion from "./Components/SimplyScansion";
 import AddPoemInterface from "./Components/AddPoemInterface";
 import Home from "./Components/Home";
 import StudentDashboard from "./Components/Dashboards/StudentDashboard";
@@ -25,7 +24,6 @@ function App() {
             <Nav.Link href="/sign-in">Log In</Nav.Link>
             <Nav.Link href="/sign-up">Sign Up</Nav.Link>
             <Nav.Link href="/how-to">How To</Nav.Link>
-            <Nav.Link href="/simply-scansion">Scan a Poem</Nav.Link>
             <Nav.Link href="/student-dashboard">Dashboard</Nav.Link>
             <Nav.Link href="/add-poem">Add a Poem</Nav.Link>
           </Nav>
@@ -39,7 +37,6 @@ function App() {
             <Route exact path='/' component={Home} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
-            <Route path='/simply-scansion' component={SimplyScansion} />
             <Route path='/student-dashboard' component={StudentDashboard} />
             <Route path='/instructor-dashboard' component={InstructorDashboard} />
             <Route path='/add-poem' component={AddPoemInterface} />
